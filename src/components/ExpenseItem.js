b42f66742144b06da7e790fe20658b8bc34f68f7
Baishali313@gmail.com <Baishali313>
2023-11-08 16:59:17 +0530

    props added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,28 @@
-function ExpenseItem(){
-    return(<div>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
+import './ExpenseItem.css'
+
+
+
+
+function ExpenseItem(props){
+
+    // const expenseDate= new Date(2021, 2, 28);
+    // const expenseTitle='Car Insurance';
+    // const expenseAmount=294.67;
+
+
+
+
+    return(<div className='expense-item'> 
+        <div>{props.date.toISOString()}</div>  
+        <div className="expense-item__description">
+            <h2>{props.title} </h2>
+            <div className='expense-item__price'>{props.amount}</div>
+            
+            
+        </div>
+       
     </div>
+    
 )
 }
      
