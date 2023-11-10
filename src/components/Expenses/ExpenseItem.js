14916b765c58a37e93106541b7ef52e3d7fc2c32
Baishali313@gmail.com <Baishali313>
2023-11-10 13:21:27 +0530

    fat arrow function added//files arranged

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,6 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import Card from '../UI/Card'
 
 
 
@@ -16,22 +17,19 @@ function ExpenseItem(props){
 
 
 
-    return(<div className='expense-item'> 
-        {/* <div>
-            <div>{month}</div>
-            <div>{date}</div>
-            <div>{year}</div>
-        </div>   */}
-        <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
-            <h2>{props.title} </h2>
-            <h2>{props.locationOfExpenses}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
+    return(
+        <Card className= 'expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title} </h2>
+                <h2>{props.locationOfExpenses}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             
             
         </div>
        
-    </div>
+    </Card>
+    
     
 )
 }
