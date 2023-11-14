13dd5fa83d7d293fe1883f8d6a6d7b759c600763
Baishali313@gmail.com <Baishali313>
2023-11-14 12:35:05 +0530

    ONCLICK EVENT LISTENER ADDED

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -28,7 +28,7 @@ function ExpenseItem(props){
                 <h2>{props.title} </h2>
                 <h2>{props.locationOfExpenses}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
-                <button onClick={clickHandler}>change title</button>
+                <button onClick={clickHandler}>Change Title</button>
             
             
         </div>
