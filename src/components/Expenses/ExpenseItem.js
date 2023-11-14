a437dc4c2811b4a021b1cd2c88405855d44fea28
Baishali313@gmail.com <Baishali313>
2023-11-14 16:01:01 +0530

    use state added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,30 +1,42 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const [isVisible, setIsVisible] = useState(true);
+  const [amount, setAmount]= useState(props.amount);
+
   const clickHandler = () => {
-    console.log('clicked!!!');
+    setTitle('updated');
+    console.log(title); // This may log the previous title due to the asynchronous nature of state updates
   };
 
   const deleteHandler = () => {
-    // Find the parent element (Card) and remove it
-    const expenseItem = document.querySelector('.expense-item');
-    expenseItem.parentNode.removeChild(expenseItem);
+    // Set isVisible to false instead of removing the element from the DOM
+    setIsVisible(false);
   };
 
-  return (
+  const changeExpenseHandler= () =>{
+    setAmount('100$');
+    
+  }
+
+  return isVisible ? (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h2>{props.locationOfExpenses}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">{amount}</div>
         <button onClick={clickHandler}>Change Title</button>
         <button onClick={deleteHandler}>Delete Expense</button>
+        <button onClick= {changeExpenseHandler}> make 100$ </button>
       </div>
     </Card>
-  );
+  ) : null;
 }
 
 export default ExpenseItem;
