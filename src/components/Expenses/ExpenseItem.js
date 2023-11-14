e44de9b316e8caec2331c71487e19091646e39e4
Baishali313@gmail.com <Baishali313>
2023-11-14 13:15:31 +0530

    delete button added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,43 +1,30 @@
 import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
-import Card from '../UI/Card'
+import './ExpenseItem.css';
+import Card from '../UI/Card';
 
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log('clicked!!!');
+  };
 
+  const deleteHandler = () => {
+    // Find the parent element (Card) and remove it
+    const expenseItem = document.querySelector('.expense-item');
+    expenseItem.parentNode.removeChild(expenseItem);
+  };
 
-
-function ExpenseItem(props){
-
-    // const expenseDate= new Date(2021, 2, 28);
-    // const expenseTitle='Car Insurance';
-    // const expenseAmount=294.67;
-    // const month=props.date.toLocaleString('en-US',{month:'long'});
-    // const date=props.date.toLocaleString('en-US',{date:'2-digit'});
-    // const year= props.date.getFullYear();
-
-    const clickHandler= ()=>{
-        console.log('clicked!!!');
-    }
-
-
-
-
-    return(
-        <Card className= 'expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title} </h2>
-                <h2>{props.locationOfExpenses}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
-                <button onClick={clickHandler}>Change Title</button>
-            
-            
-        </div>
-       
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.locationOfExpenses}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteHandler}>Delete Expense</button>
+      </div>
     </Card>
-    
-    
-)
+  );
 }
-     
 
 export default ExpenseItem;
