5bfc521b698c4f730f79d174331a47eaf6780a4c
Baishali313@gmail.com <Baishali313>
2023-11-16 18:12:47 +0530

    lifting the states

--- src/App.js
+++ src/App.js
@@ -1,59 +1,56 @@
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import NewExpense from './components/NewExpense/NewExpense';
-const App= ()=> {
-  const expenses = [
+import React, { useState } from 'react';
+
+const App = () => {
+  // Initial state with your expenses array
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      // locationOfExpenses: 'dmart, delhi'
     },
-    { id: 'e2', 
-    title: 'New TV', 
-    amount: 799.49,
-     date: new Date(2021, 2, 12)},
-  //  locationOfExpenses:'lg showroom,delhi' },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      // locationOfExpenses:'insurance office, delhi'
-
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
-      // locationOfExpenses:'ikea, delhi'
     },
-  ];
+  ]);
 
-  
-      const expenseComponent=[];
-      for(let i=0;i<expenses.length;i++){
-     
-        expenseComponent.push(
+  const expenseComponents = expenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    ></ExpenseItem>
+  ));
 
-         <ExpenseItem 
-         title={expenses[i].title}
-         amount={expenses[i].amount}
-         date={expenses[i].date}
-        //  locationOfExpenses={expenses[i].locationOfExpenses}
-         ></ExpenseItem>
-      )};
-      
-    
-
+  const addExpenseHandler = (expense) => {
+    // Use the spread operator to create a new array with the existing expenses and the new expense
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
 
   return (
     <div>
-      <NewExpense></NewExpense>
-      {expenseComponent}
-    </div>  
-  );    
-}
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      {expenseComponents}
+    </div>
+  );
+};
 
 export default App;
